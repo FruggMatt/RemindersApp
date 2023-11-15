222d7d63b596b231854200ccb9e3328d75a0173f
FruggMatt <148440360+FruggMatt@users.noreply.github.com>
2023-11-15 09:58:42 +0100

    Initial Commit

---
+++ ReminderApp/ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Fruggiero Mattia on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
